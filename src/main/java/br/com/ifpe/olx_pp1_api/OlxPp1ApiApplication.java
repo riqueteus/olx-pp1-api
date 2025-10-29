969e7bd702a9e7b66169895bf2bd6aa0280ed24f
debb <debora.leticia11.dl@gmail.com>
2025-10-28 22:07:50 -0300

    primeiro commit

---
+++ src/main/java/br/com/ifpe/olx_pp1_api/OlxPp1ApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.olx_pp1_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlxPp1ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlxPp1ApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ifpe/olx_pp1_api/OlxPp1ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.olx_pp1_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlxPp1ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
